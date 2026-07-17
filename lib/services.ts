6ffd38e7ac85067c8c3286a6e6956cf770c31e73
root <root@SRV-CT-GLPI.SRV-CT-GLPI>
2026-07-17 05:01:19 +0000

    change hostname nextcloud

--- lib/services.ts
+++ lib/services.ts
@@ -62,7 +62,7 @@ export const categories: ServiceCategory[] = [
       {
         name: 'Nextcloud',
         description: 'Penyimpanan & Berbagi Dokumen Antar Unit',
-        url: 'http://192.168.105.9:8081',
+        url: 'https://nextcloud.brawijayahospitaltangerang.com',
         host: 'http://nextcloud.lan',
         icon: Cloud,
         status: 'online',
